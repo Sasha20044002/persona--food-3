30828df1aa3b41582dcad5911df892d7fad37e27
Sca1red <40990563+Sasha20044002@users.noreply.github.com>
2021-08-09 14:14:30 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-$("#range").slider({
-  animate: "slow",
-  range: "min",
-  value: 50,
-});
-
-$("#range").draggable();
+function range() {
+  var val = $(".discount__range__item").val();
+  $(".discount__range__item").css({
+    background:
+      "-webkit-linear-gradient(left,#85af27 0%,#85af27 " +
+      val +
+      "%,#ffffff " +
+      val +
+      "%,#ffffff 100%)",
+  });
+}
